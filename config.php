48c1ae4ad803782f937379173ae0c49171e206a0
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2022-08-21 22:15:02 +0500

    remove client id and secrit

--- config.php
+++ config.php
@@ -1,18 +1,9 @@
 <?php
-// return array(
-//     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
-//     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-//     'client_id' => 'AB7NBxrVcIBS6NrwnvA1yFQNIf6pZahjnXncRAYNO93QC9RM5A',
-//     'client_secret' => 'ObVVPz1HgakW6PTdmOWETjKiem75LGO3SkDqqUHB',
-//     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
-//     'oauth_redirect_uri' => 'http://localhost/HelloWorld-PHP/callback.php',
-// )
-
 return array(
     'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
     'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
-    'client_id' => 'AB1TyPz9OIK4i1uVLVEn7YMAd2z8WmsJTOy04tS48eNT4yQ3f5',
-    'client_secret' => 'e4FXJ5s4Lk8PQWlAfMFa4OgfWB9OpRYGqy7WtewQ',
+    'client_id' => '',
+    'client_secret' => '',
     'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
     'oauth_redirect_uri' => 'http://localhost/HelloWorld-PHP/callback.php',
 )
