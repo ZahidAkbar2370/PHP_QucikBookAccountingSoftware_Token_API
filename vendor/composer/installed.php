4aa2c498b1119519754276f3e0861c2e96b103d9
ZahidAkbar2370 <zahidjakhar2370@gmail.com>
2022-08-21 22:11:38 +0500

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'intuitdeveloper/helloworld-php',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '06cb60d8505c4fea9eec9b2e70e1b7fa7370368e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'intuitdeveloper/helloworld-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '06cb60d8505c4fea9eec9b2e70e1b7fa7370368e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'quickbooks/v3-php-sdk' => array(
+            'pretty_version' => '6.0.5',
+            'version' => '6.0.5.0',
+            'reference' => 'a2cc23508c8b1c1d8c595d249e5c114917a2f65d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../quickbooks/v3-php-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
